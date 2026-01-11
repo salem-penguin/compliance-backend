9f75350fffdf8dd2d53aee8ff7486c248df765e3
salem-penguin <s.alnsour@penguinin.com>
2026-01-11 10:57:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1920,7 +1920,7 @@ app = FastAPI(title="Compliance Table Backend", version="1.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*","https://compliance-8ul.pages.dev"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -3490,5 +3490,6 @@ if __name__ == "__main__":
 
     #uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
     uvicorn.run("main:app", host="127.0.0.1", port=8000)
+
 
 
